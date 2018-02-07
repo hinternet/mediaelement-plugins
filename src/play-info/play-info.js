2f18bd0b4e6c1777d753436426f6b3c8714695c8
Iñigo Gascon Royo <igascon@hiberus.local>
2018-02-07 11:19:12 +0100

    Modify links to open in new tab

--- src/play-info/play-info.js
+++ src/play-info/play-info.js
@@ -107,9 +107,9 @@ Object.assign(MediaElementPlayer.prototype, {
             item = document.createElement('li'),
             id = `${t.id}_play-info_0`,
             thumbnail = element['data-play-info-thumbnail'] ? `<div class="${t.options.classPrefix}play-info-item-thumbnail"><img tabindex="-1" src="${element['data-play-info-thumbnail']}"></div>` : '',
-            description = element['data-play-info-description-link'] ? `<a href="${element['data-play-info-description-link']}"><div class="${t.options.classPrefix}play-info-item-description-linked">${element['data-play-info-description']}</div></a>` :
+            description = element['data-play-info-description-link'] ? `<a target="_blank" href="${element['data-play-info-description-link']}"><div class="${t.options.classPrefix}play-info-item-description-linked">${element['data-play-info-description']}</div></a>` :
                 (element['data-play-info-description'] ? `<div class="${t.options.classPrefix}play-info-item-description">${element['data-play-info-description']}</div>` : ''),
-            title = element['title-link'] ? `<a href="${element['title-link']}">${element.title}</a>` : element.title
+            title = element['title-link'] ? `<a target="_blank" href="${element['title-link']}">${element.title}</a>` : element.title
         ;
         item.tabIndex = 0;
         item.className = `${t.options.classPrefix}play-info-selector-list-item ${t.options.classPrefix}play-info-selected`;
